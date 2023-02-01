2c90494b6e7b0642b6197a8c88e988e8313a70ea
Benga <benga.olasebikan@gmail.com>
2023-02-01 21:07:48 +0000

    Navbar Footer and Layout added

---
+++ components/libs/helpers.ts
@@ -0,0 +1,48 @@
+export const navLinks = [
+  {
+    name: 'Home',
+    link: '/',
+    img: '//cdn.jsdelivr.net/npm/heroicons@1.0.1/outline/home.svg'
+  },
+  {
+    name: 'About',
+    link: '/about',
+    img: '//cdn.jsdelivr.net/npm/heroicons@1.0.1/outline/user.svg'
+  },
+  {
+    name: 'Resume',
+    link: '/resume',
+    img: '//cdn.jsdelivr.net/npm/heroicons@1.0.1/outline/newspaper.svg'
+  },
+  {
+    name: 'GitHub',
+    link: 'https://github.com/BengaCodes',
+    img: 'https://raw.githubusercontent.com/rdimascio/icons/932c4cf6c9e2031abeca1c164baa0f76785c16fe/icons/github.svg'
+  },
+  {
+    name: 'LinkedIn',
+    link: 'https://www.linkedin.com/in/benga-olasebikan',
+    img: 'https://raw.githubusercontent.com/rdimascio/icons/932c4cf6c9e2031abeca1c164baa0f76785c16fe/icons/linkedin.svg'
+  }
+]
+
+export const footerLinks = [
+  {
+    name: 'Resume',
+    link: '/resume'
+  },
+  {
+    name: 'GitHub',
+    link: 'https://github.com/BengaCodes'
+  },
+  {
+    name: 'LinkedIn',
+    link: 'https://www.linkedin.com/in/benga-olasebikan'
+  },
+  {
+    name: 'About',
+    link: '/about'
+  }
+]
+
+export const navTargetBlankLinks = ['GitHub', 'LinkedIn']
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,6 @@ import Navbar from 'components/Navbar'
 
 export default function Home() {
   return (
-    <h1 className='text-3xl font-bold underline text-red-400'>
-      Hello world!
-      <Navbar />
-    </h1>
+    <h1 className='text-3xl font-bold underline text-red-400'>Hello world!</h1>
   )
 }
