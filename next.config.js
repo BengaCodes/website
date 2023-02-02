e6701b14d8713e9ac88343a3aa16d5fe48fe03f0
Benga <benga.olasebikan@gmail.com>
2023-02-02 13:33:12 +0000

    Resume displayed on page and bg changed

--- next.config.js
+++ next.config.js
@@ -1,8 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    appDir: true,
+    appDir: true
   },
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    config.resolve.alias.canvas = false
+    config.resolve.alias.encoding = false
+    return config
+  }
 }
 
 module.exports = nextConfig
